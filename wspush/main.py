3361265310b526d7a8ed495d90323434c1b0549f
codeif <me@codeif.com>
2018-06-06 19:06:16 +0800

    websocket接口，随机生成数据

--- wspush/main.py
+++ wspush/main.py
@@ -1,14 +1,59 @@
+import asyncio
+import random
+
 from aiohttp import web
 
-from .views import index
+from .views import hello, websocket_handler
+
+
+async def send_data(app):
+    try:
+        while True:
+            data = [round(random.random(), 2) for _ in range(6)]
+            for ws in app['websockets']:
+                await ws.send_json(data)
+            await asyncio.sleep(random.random() * 10)
+    except asyncio.CancelledError:
+        pass
+
+
+async def start_background_tasks(app):
+    app['ws_listener'] = app.loop.create_task(send_data(app))
+
+
+async def cleanup_background_tasks(app):
+    app['ws_listener'].cancel()
+    await app['ws_listener']
 
 
 async def init_app():
     app = web.Application()
-    app.add_routes([web.get('/', index)])
+
+    app['websockets'] = set()
+
+    app.on_startup.append(start_background_tasks)
+    app.on_cleanup.append(cleanup_background_tasks)
+    app.on_shutdown.append(shutdown)
+
+    app.add_routes([
+        web.get('/', websocket_handler),
+        web.get('/hello', hello),
+    ])
+
     return app
+
+
+async def shutdown(app):
+    while True:
+        try:
+            ws = app['websockets'].pop()
+            await ws.close()
+        except KeyError:
+            break
+
+    print(len(app['websockets']))
 
 
 def main():
     app = init_app()
-    web.run_app(app)
+    web.run_app(app, port=5441)
--- wspush/views.py
+++ wspush/views.py
@@ -1,5 +1,26 @@
+import aiohttp
 from aiohttp import web
 
 
-async def index(request):
+async def hello(request):
     return web.Response(text="Hello, world")
+
+
+async def websocket_handler(request):
+
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    request.app['websockets'].add(ws)
+    async for msg in ws:
+        print('this is msg.type', msg.type)
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws.close()
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print('ws connection closed with exception %s' %
+                  ws.exception())
+
+    print('websocket connection closed')
+    request.app['websockets'].discard(ws)
+    return ws
